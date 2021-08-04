6df655c420c3f24d573044c3d39e49170e5b04ed
tan27 <tn2706@gmail.com>
2021-08-04 16:07:53 -0700

    hash

--- server.js
+++ server.js
@@ -13,7 +13,9 @@ const image = require('./controllers/image');
 const db = knex ({
     client: 'pg',
     connectionString: process.env.DATABASE_URL,
-    ssl: true
+    ssl: {
+    rejectUnauthorized: false
+    }
   });
 
 const app = express();
