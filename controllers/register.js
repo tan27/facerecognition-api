04ac7fda416733133411e202bef8c1d10083e61f
tan27 <tn2706@gmail.com>
2021-08-04 01:10:54 -0700

    edits

--- controllers/register.js
+++ controllers/register.js
@@ -3,7 +3,6 @@ const handleRegister = (req, res, db, bcrypt) => {
     if (!email || !name || !password) {
         return res.status(400).json('incorrect form submission');
     }
-    const { email, name, password } = req.body;
     const hash = bcrypt.hashSync(password);
       db.transaction(trx => {
         trx.insert({
