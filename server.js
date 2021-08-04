781f5048bc89c1c90222476c2e4d67f10237bd82
tan27 <tn2706@gmail.com>
2021-08-04 16:36:46 -0700

    hash

--- server.js
+++ server.js
@@ -19,6 +19,14 @@ const db = knex ({
 
   db.connect();
 
+  db.query('SELECT table_schema,table_name FROM information_schema.tables;', (err, res) => {
+    if (err) throw err;
+    for (let row of res.rows) {
+      console.log(JSON.stringify(row));
+    }
+    db.end();
+  });
+
 const app = express();
 app.use(express.urlencoded({extended: false}));
 app.use(express.json())
