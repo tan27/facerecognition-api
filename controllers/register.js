552faea232773afe4eb718bb020464b6d1fec048
tan27 <tn2706@gmail.com>
2021-08-04 17:01:10 -0700

    hash

--- controllers/register.js
+++ controllers/register.js
@@ -4,29 +4,29 @@ const handleRegister = (req, res, db, bcrypt) => {
         return res.status(400).json('incorrect form submission');
     }
     const hash = bcrypt.hashSync(password, bcrypt.genSaltSync(10));
-      db.transaction(trx => {
-        trx.insert({
-          hash: hash,
-          email: email
-        })
-        .into('login')
-        .returning('email')
-        .then(loginEmail => {
-          return trx('users')
-            .returning('*')
-            .insert({
-              email: loginEmail[0],
-              name: name,
-              joined: new Date()
-            })
-            .then(user => {
-              res.json(user[0]);
-            })
-        })
-        .then(trx.commit)
-        .catch(trx.rollback)
+    db.transaction(trx => {
+      trx.insert({
+        hash: hash,
+        email: email
       })
-      .catch(err => res.status(400).json('unable to register'))
-  }
+      .into('login')
+      .returning('email')
+      .then(loginEmail => {
+        return trx('users')
+          .returning('*')
+          .insert({
+            email: loginEmail[0],
+            name: name,
+            joined: new Date()
+          })
+          .then(user => {
+            res.json(user[0]);
+          })
+      })
+      .then(trx.commit)
+      .catch(trx.rollback)
+    })
+    .catch(err => res.status(400).json('unable to register'))
+}
 
 export default handleRegister;
